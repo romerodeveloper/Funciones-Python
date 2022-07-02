a4ec8476e9d067d2110a2343599dae4562f7b6c1
Nicolas Romero <desarrollador.romero@gmail.com>
2022-07-01 23:25:13 -0500

    avance poo

---
+++ POO/claseejemplo.py
@@ -0,0 +1,11 @@
+# Hoja de ejemplos para clases, atributos y metodos
+
+class Torta:
+    chocolate = False
+
+torta_2 = Torta()
+
+torta_2.chocolate = True
+
+print(f"tiene chocolate? {torta_2.chocolate}")
+
